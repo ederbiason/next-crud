bd5c10ef450272d7f35d60c7fa24b244b9cf33cf
Eder Henrique Biason de Oliveira <82395795+ederbiason@users.noreply.github.com>
2022-08-09 15:20:34 -0300

    Header and settings
    Done the header component using icons from Heroicons and made the header responsive
    
    Some settings in the structure of the project

---
+++ src/components/HeaderItem.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+interface IProps { 
+    title: string;
+    Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+}
+
+export const HeaderItem = ({title, Icon}: IProps) => {
+  return (
+    <div className="flex flex-col items-center cursor-pointer w-12 group sm:w-20 hover:text-white">
+        <Icon 
+            className='h-8 mb-1 group-hover:animate-bounce' 
+        />
+
+        <p className='opacity-0 group-hover:opacity-100 tracking-widest'>
+            {title}
+        </p>
+    </div>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { Header } from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Hulu Clone</title>
+      </Head>
+
+      <Header />
+
+      
+
+    </div>
+  )
+}
+
+export default Home
